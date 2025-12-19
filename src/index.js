7e3462760c94e4fef8b82d75a8a77ba5c30e89e0
Taras <chirkovtaras@gmail.com>
2025-12-19 16:57:18 +0200

    feat: add regions

--- src/index.js
+++ src/index.js
@@ -78,7 +78,7 @@ function extractAppId(url) {
 function getNextRunAtUTC(now = new Date()) {
   const minutes = now.getUTCMinutes();
   const hours = now.getUTCHours();
-  const slots = [5, 15, 25, 35, 45, 55];
+  const slots = [0, 10, 20, 30, 40, 50];
 
   for (const m of slots) {
     if (minutes < m) {
